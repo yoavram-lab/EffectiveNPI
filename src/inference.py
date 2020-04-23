38b24d6ad87d38c55a699e3258f6ad38df591e8d
adsurbum <adsurbum@gmail.com>
2020-04-23 19:11:26 +0300

    merging refactoring

--- src/inference.py
+++ src/inference.py
@@ -1,6 +1,3 @@
-# TODO change filename to inference?
-# TODO remove any function that is not used here?
-# TODO rename to "model.py"?
 import urllib.request
 import os
 from datetime import timedelta, datetime
@@ -14,6 +11,7 @@ import pandas as pd
 from scipy.integrate import odeint
 import scipy.stats
 import emcee
+import argparse
 
 
 Td1 = 9
@@ -27,7 +25,7 @@ def find_start_day(cases_and_dates):
     for r in cases_and_dates.iterrows():
         curr_cases = r[1]['cases']
         if last_is_not_zero and curr_cases != 0:
-			break
+            break
         last_is_not_zero = False if curr_cases == 0 else True
         if curr_cases == 0:
             last_date = r[1]['date']
@@ -91,11 +89,6 @@ def log_likelihood(θ, X):
     Z, D, μ, β, α1, λ, α2, E0, Iu0, τ = θ
     τ = int(τ)
     
-    # TODO the following is implemented in log_prior and should not be here
-    case = (2 <= Z <=5) and (2 <= D <= 5) and (0.2 <= μ <= 1) and (0.8 <= β <= 1.5) and (0.02 <= α1 <= 1) and (0 <= λ <= 1) and (0.02 <= α2 <= 1) and (0 < E0 < seed_max) and (0 < Iu0 < seed_max) and (1<=τ<=ndays-1)
-    if not case:
-        return -np.inf
-    
     S, E, Ir, Iu, R = simulate(*θ)
     p1 = 1/Td1
     p2 = 1/Td2
@@ -112,7 +105,9 @@ def log_likelihood(θ, X):
 
 
 def log_posterior(θ, X):
-    logpri = log_prior(θ)     
+    logpri = log_prior(θ)  
+    if np.isinf(logpri): 
+        return logpri   
     assert not np.isnan(logpri), (logpri, θ)
     loglik = log_likelihood(θ, X)
     assert not np.isnan(loglik), (loglik, θ)
@@ -121,50 +116,55 @@ def log_posterior(θ, X):
 
 
 if __name__ == '__main__':
-	country = sys.argv[1] # TODO if there is no argv[1], print usage message
+    parser = argparse.ArgumentParser()
+    parser.add_argument('country_name')
+    parser.add_argument('-s', '--steps',type=int,help='you can provide number of iteration steps, othewise the default is taken')
+    args = parser.parse_args()
+    country = args.country_name
 
-	if country=='Wuhan':
-	    df = pd.read_csv('../data/Incidence.csv')
-	    df['date'] = pd.to_datetime(df['Date'], dayfirst=True)
-	    df['cases'] = df['Wuhan']
-	    df = df[::-1] # TODO why?
-	    N = pd.read_csv('../data/pop.csv', index_col='City').loc['Wuhan'].values[0]
-	else:
-	    url = 'https://github.com/ImperialCollegeLondon/covid19model/raw/v1.0/data/COVID-19-up-to-date.csv'
-	    fname = '../data/COVID-19-up-to-date.csv'
-	    if not os.path.exists(fname):
-	        urllib.request.urlretrieve(url, fname)
-	    df = pd.read_csv(fname, encoding='iso-8859-1')
-	    df['date'] = pd.to_datetime(df['dateRep'], format='%d/%m/%Y')
-	    df = df[df['countriesAndTerritories']==country]
-	    N = df.iloc[0]['popData2018']
+    if country=='Wuhan':
+        df = pd.read_csv('../data/Incidence.csv')
+        df['date'] = pd.to_datetime(df['Date'], dayfirst=True)
+        df['cases'] = df['Wuhan']
+        df = df[::-1] # TODO why?
+        N = pd.read_csv('../data/pop.csv', index_col='City').loc['Wuhan'].values[0]
+    else:
+        url = 'https://github.com/ImperialCollegeLondon/covid19model/raw/v1.0/data/COVID-19-up-to-date.csv'
+        fname = '../data/COVID-19-up-to-date.csv'
+        if not os.path.exists(fname):
+            urllib.request.urlretrieve(url, fname)
+        df = pd.read_csv(fname, encoding='iso-8859-1')
+        df['date'] = pd.to_datetime(df['dateRep'], format='%d/%m/%Y')
+        df = df[df['countriesAndTerritories']==country]
+        N = df.iloc[0]['popData2018']
 
-	cases_and_dates = df.iloc[::-1][['cases','date']]
-	start_date = find_start_day(cases_and_dates)
-	X = np.array(cases_and_dates[cases_and_dates['date']>=start_date]['cases'])
-	ndays = len(X)
+    cases_and_dates = df.iloc[::-1][['cases','date']]
+    start_date = find_start_day(cases_and_dates)
+    X = np.array(cases_and_dates[cases_and_dates['date']>=start_date]['cases'])
+    ndays = len(X)
 
-	var_names = ['Z', 'D', 'μ', 'β', 'α1', 'λ', 'α2', 'E0', 'Iu0','τ']
-	ndim = len(var_names)
-	nwalkers = 50
-	nsteps = 500 * 3 * 50
-	nsteps = 10 * 50 # TODO remove this line or the former
-	# nburn = nsteps // 2
-	guesses = np.array([prior() for _ in range(nwalkers)])
+    var_names = ['Z', 'D', 'μ', 'β', 'α1', 'λ', 'α2', 'E0', 'Iu0','τ']
+    ndim = len(var_names)
+    nwalkers = 50
+    nsteps = 500 * 3 * 50
+    if args.steps:
+        nsteps = args.steps
 
-	np.random.seed(10)
-	# with Pool() as pool: # TODO maybe use ProcessPoolExecutor?
-	sampler = emcee.EnsembleSampler(nwalkers, ndim, log_posterior, args=[X])
-	sampler.run_mcmc(guesses, nsteps, progress=True);
+    # nsteps = 10 * 50 # TODO remove this line or the former
+    guesses = np.array([prior() for _ in range(nwalkers)])
 
-	params = [nsteps, ndim, int(N), Td1, Td2]
-	np.savez_compressed(
-		# TODO pwd is 'src', save to ../output/<country>/<date>_<time>_inference.npz
-		'./outputs/test/data/{}.npz'.format(country), 
-		chain=sampler.chain, # TODO what about the burnin?
-		incidences=X, # TODO maybe save as X=X
-		params=params, 
-		var_names=var_names,
-		start_date=str(start_date)
-	)
+    np.random.seed(10)
+    with Pool() as pool:
+        sampler = emcee.EnsembleSampler(nwalkers, ndim, log_posterior, args=[X])
+        sampler.run_mcmc(guesses, nsteps, progress=True);
+
+    params = [nsteps, ndim, int(N), Td1, Td2]
+    np.savez_compressed(
+        './output/test/data/{}.npz'.format(country), #we copy the results to the production dir after
+        chain=sampler.chain,
+        incidences=X, # TODO maybe save as X=X
+        params=params, 
+        var_names=var_names,
+        start_date=str(start_date)
+    )
 
