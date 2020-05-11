ffc5220af19cea1a95758fedc5990732cd7992dc
Yoav Ram <yoav@yoavram.com>
2020-05-11 15:07:16 +0300

    create table csv and tex from csv output

---
+++ src/Table2.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+import os
+import sys
+
+sns.set_context('paper', font_scale=1.3)
+red, blue, green = sns.color_palette('Set1', 3)
+
+def int_to_dt(t):
+    return pd.to_datetime(start_date) + timedelta(days=t)
+
+if __name__ == '__main__':
+	job_id = sys.argv[1]
+	output_folder = r'/Users/yoavram/Library/Mobile Documents/com~apple~CloudDocs/EffectiveNPI-Data/output/{}/'.format(job_id)
+
+	NPI_dates = pd.read_csv('../data/NPI_dates.csv')
+
+	data = pd.read_csv(os.path.join(output_folder, 'tables', 'all-countries-{}.csv'.format(job_id)))
+	features = ['country', 'official_τ', 'τ median', 'τ CI (75%)', 'τ CI (95%)']
+	features += [x for x in data.columns if 'median' in x and 'τ' not in x]
+
+	table = data[features].copy()
+	table['country'] = [x.replace('_', ' ') for x in table['country']]
+	table.columns = [x.replace(' median', '') for x in table.columns]
+	table = table.rename(columns=
+	    {'country': 'Country', 'official_τ': r'$\tau^*$', 'τ' : r'$\tau$', 'τ CI (75%)': '75\% CI', 'τ CI (95%)': '95\% CI', 
+	     'Δt0': r'$\Delta t$', 'E0': r'$E(0)$', 'Iu0': r'$I_u(0)$', 'μ': r'$\mu$', 'β': r'$\beta$', 'λ': r'$\lambda$', 
+	     'α1': r'$\alpha_1$', 'α2': r'$\alpha_2$', 'Z': '$Z$', 'D': '$D$'
+	    })
+	table.columns = ["\ccell{"+x+"}" for x in table.columns]
+
+	csv_filename = '../figures/Table2.csv'
+	tex_filename = '../figures/Table2.tex'
+	table.to_csv(csv_filename, index=False)
+	with open(tex_filename, 'wt') as f:
+	    print(table.to_latex(escape=False, index=False), file=f)
+	print("Saved output to {} and {}".format(csv_filename, tex_filename))
