3cd6097fa04d8d5be3fda9bfaafe96f4863a1a21
adsurbum <adsurbum@gmail.com>
2021-01-21 01:15:38 +0200

    improved Fig-autocorr

--- src/Fig_autocorr.py
+++ src/Fig_autocorr.py
@@ -16,24 +16,25 @@ class FixedScalarFormatter(matplotlib.ticker.ScalarFormatter):
         self.orderOfMagnitude = self.power
         
 def plot_auto(country_name): 
-    file_name = '7M/inference/{}.autocorr'.format(country_name)
+    file_name = '7MFixed/inference/{}.autocorr'.format(country_name)
     auto = np.loadtxt(file_name)
     iters = np.array([0]+[i*200000 for i in range(1,len(auto)+1)])/1_000_000
     taus = np.array([0]+list(auto))/1000
-    print(max(taus))
+    print(max(taus),country_name)
 #     plt.plot(iters, iters/100.0,'--k')
     plt.plot(iters, taus, label=country_name)
-    plt.xlabel("number of iterations")
-    plt.ylabel(r"auto-correlation mean");
+    plt.xlabel("Samples (1M)")
+    plt.ylabel("Average IAT (1K)");
     lgd = plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
     plt.xlim(0,iters[-1])
     return lgd
 
 for c in ['Austria','Belgium','Denmark','France','Germany','Italy','Norway',
-          'Spain','Sweden','Switzerland','United_Kingdom','Wuhan']:
+          'Spain','Sweden','Switzerland','United_Kingdom']:
     lgd = plot_auto(c)
 plt.ylim(0)
 
 # plt.gca().xaxis.set_major_formatter(FixedScalarFormatter(6)) #doesn't work :\\\\
 # plt.gca().yaxis.set_major_formatter(FixedScalarFormatter(3)) 
-plt.gcf().savefig('7M/figures/Fig-autocorr.pdf',bbox_extra_artists=(lgd,), bbox_inches='tight')
+# plt.tight_layout()
+plt.gcf().savefig('7MFixed/figures/Fig-autocorr.pdf',bbox_extra_artists=(lgd,), bbox_inches='tight')
