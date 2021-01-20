c3178850174e3217f63a33782b8446c52490bf98
adsurbum <adsurbum@gmail.com>
2021-01-21 00:07:43 +0200

    more changes on plots

--- src/Fig_autocorr.py
+++ src/Fig_autocorr.py
@@ -18,20 +18,22 @@ class FixedScalarFormatter(matplotlib.ticker.ScalarFormatter):
 def plot_auto(country_name): 
     file_name = '7M/inference/{}.autocorr'.format(country_name)
     auto = np.loadtxt(file_name)
-    iters = np.array([0]+[i*200000 for i in range(1,len(auto)+1)])
-    taus = np.array([0]+list(auto))
+    iters = np.array([0]+[i*200000 for i in range(1,len(auto)+1)])/1_000_000
+    taus = np.array([0]+list(auto))/1000
+    print(max(taus))
 #     plt.plot(iters, iters/100.0,'--k')
     plt.plot(iters, taus, label=country_name)
     plt.xlabel("number of iterations")
     plt.ylabel(r"auto-correlation mean");
-    plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    lgd = plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
     plt.xlim(0,iters[-1])
+    return lgd
 
 for c in ['Austria','Belgium','Denmark','France','Germany','Italy','Norway',
           'Spain','Sweden','Switzerland','United_Kingdom','Wuhan']:
-    plot_auto(c)
+    lgd = plot_auto(c)
 plt.ylim(0)
 
-plt.gca().xaxis.set_major_formatter(FixedScalarFormatter(6)) #doesn't work :\\\\
-plt.gca().yaxis.set_major_formatter(FixedScalarFormatter(3)) 
-plt.gcf().savefig('7M/figures/auto-correlation.pdf')
+# plt.gca().xaxis.set_major_formatter(FixedScalarFormatter(6)) #doesn't work :\\\\
+# plt.gca().yaxis.set_major_formatter(FixedScalarFormatter(3)) 
+plt.gcf().savefig('7M/figures/Fig-autocorr.pdf',bbox_extra_artists=(lgd,), bbox_inches='tight')
--- src/Fig_trace_tau.py
+++ src/Fig_trace_tau.py
@@ -3,52 +3,39 @@ import matplotlib.pyplot as plt
 import matplotlib
 from matplotlib.ticker import ScalarFormatter
 import seaborn as sns
-sns.set_context('paper', font_scale=1.3)
 import sys
+from datetime import timedelta, datetime
+import pandas as pd
 
-def plot_τ_trace(path, country_name):
+sns.set_context('paper', font_scale=1.6)
+
+
+def τ_to_string(τ,start_date):
+    return (pd.to_datetime(start_date) + timedelta(days=τ)).strftime('%b %d')
+
+def plot_τ_trace(path, country_name, thin=10_000, alpha=0.1):
     file_name_npz = '{}/inference/{}.npz'.format(path, country_name)
     data = np.load(file_name_npz)
     chain = data['chain']
     var_names = list(data['var_names'])
+    start_date = data['start_date']
+    maxx = len(data['incidences'])-2
+    minn = 10
     
     fig, ax = plt.subplots()
-    plt.plot(chain[:,::100,var_names.index('τ')].T, color='k', alpha=0.15);
+    plt.plot(chain[:,::thin,var_names.index('τ')].T, alpha=alpha);
     labels = ['−10000', '0', '1', '2', '3', '4', '5', '6', '7', '8']
     ax.set_xticklabels(labels);
-    ax.text(x = 0.93, y = -0.15, s = '1e6', transform=ax.transAxes);
-    plt.xlim(-2000, 70000);
-    plt.xlabel('number of iterations')
+    plt.xlim(-2_00_000/thin, 7_000_000/thin);
+    plt.ylim(minn,maxx)
+    plt.xlabel('Samples (1M)')
     plt.ylabel('τ')
     plt.title(country_name);
+    days = np.linspace(minn,maxx,5)
+    labels = [τ_to_string(d,start_date) for d in days]
+    plt.yticks(days,labels);
     plt.tight_layout()
-    fig.savefig('{}/figures/{}_trace1.png'.format(path, country_name),dpi=100)
-    
-    fig, ax = plt.subplots()
-    plt.plot(chain[:,::100,var_names.index('τ')].T, color='k', alpha=0.05);
-    labels = ['−10000', '0', '1', '2', '3', '4', '5', '6', '7', '8']
-    ax.set_xticklabels(labels);
-    ax.text(x = 0.93, y = -0.15, s = '1e6', transform=ax.transAxes);
-    plt.xlim(-2000, 70000);
-    plt.xlabel('number of iterations')
-    plt.ylabel('τ')
-    plt.title(country_name);
-    plt.tight_layout()
-    fig.savefig('{}/figures/{}_trace2.png'.format(path, country_name),dpi=100)
-    
-    fig, ax = plt.subplots()
-    plt.plot(chain[:,::100,var_names.index('τ')].T);
-    # fig.canvas.draw()
-    # labels = [item.get_text() for item in ax.get_xticklabels()]
-    labels = ['−10000', '0', '1', '2', '3', '4', '5', '6', '7', '8']
-    ax.set_xticklabels(labels);
-    ax.text(x = 0.93, y = -0.15, s = '1e6', transform=ax.transAxes);
-    plt.xlim(-2000, 70000);
-    plt.xlabel('number of iterations')
-    plt.ylabel('τ')
-    plt.title(country_name);
-    plt.tight_layout()
-    fig.savefig('{}/figures/{}_trace3.png'.format(path, country_name),dpi=100)
+    fig.savefig('{}/figures/{}_trace.pdf'.format(path, country_name),dpi=100)
     
 path = './'
 job_id = sys.argv[1]
